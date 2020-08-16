430b4f844baaee484314ed7c1315ba780af27337
Corentin <corentin.tho@gmail.com>
2020-08-16 19:59:16 +0200

    feat: base files & config

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(config.token);
