dca54d3bfbd10e46f7f14b355ead7987334b450a
Corentin <corentin.tho@gmail.com>
2020-08-19 07:01:15 +0200

    feat: remove translations

--- commands/game/villagers.js
+++ commands/game/villagers.js
@@ -2,22 +2,14 @@ const root = require('app-root-path');
 const fs = require('fs');
 const Discord = require('discord.js');
 
-const { translatePersonality, translateSpecies, translateHobbies } = require(root + '/src/translations/translations-EUfr');
-
 /**
  * @param villager
  */
 function description(villager) {
 	let birthdate = villager['birthday'].split('/');
 	birthdate = ('0' + birthdate[0]).slice(-2) + '/' + ('0' + birthdate[1]).slice(-2);
-
-	if (villager['gender'] === 'Female') {
-		return `${villager['name']['name-EUfr']} est une habitante de personnalité \`${translatePersonality(villager['personality']).toLowerCase()}\`, née le ${birthdate}`;
-	}
-
-	if (villager['gender'] === 'Male') {
-		return `${villager['name']['name-EUfr']} est un habitant de personnalité \`${translatePersonality(villager['personality']).toLowerCase()}\`, né le ${birthdate}`;
-	}
+	const gender = villager['gender'];
+	return `${villager['name']['name-EUfr']} est une habitant${gender === 'Male' ? '' : 'e'} de personnalité \`${villager['personality'].toLowerCase()}\`, né${gender === 'Male' ? '' : 'e'} le ${birthdate}`;
 }
 
 module.exports = {
@@ -43,12 +35,12 @@ module.exports = {
 						},
 						{
 							name: 'Espèce',
-							value: translateSpecies(villager['species']),
+							value: villager['species'],
 							inline: true,
 						},
 						{
 							name: 'Hobby',
-							value: translateHobbies(villager['hobby']),
+							value: villager['hobby'],
 							inline: true,
 						},
 					)
